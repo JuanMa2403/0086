def768872604551ba0e94c23280d336f960e5500
juanma1 <jmaciclodam@gmail.com>
2014-03-24 09:05:39 +0100

    0086: Metodo generateResponse() completo

--- Responder.java
+++ Responder.java
@@ -5,13 +5,26 @@
  * @author     Michael Kölling and David J. Barnes
  * @version    0.1 (2011.07.31)
  */
+import java.util.Random;
+import java.util.ArrayList;
 public class Responder
 {
+    private ArrayList<String> frases;
+    private Random aleatorio;
     /**
      * Construct a Responder - nothing to do
      */
+    
     public Responder()
     {
+        aleatorio=new Random();
+        frases= new ArrayList<String>();
+        frases.add("La respuesta es 42");
+        frases.add("No tengo constancia de eso");
+        frases.add("Escribe una carta a Santa");
+        frases.add("Colabora con la ORG: Que me den pa un piso");
+        frases.add("Dos escarabajos entran en un bar...");
+        frases.add("La ultima frase");
     }
 
     /**
@@ -20,7 +33,9 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        int alAzar=0;
+        alAzar=aleatorio.nextInt(6);
+        return frases.get(alAzar);
     }
 }
 
